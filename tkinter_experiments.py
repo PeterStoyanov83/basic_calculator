b2841fe692931c6415ba660c103f77d14c24b5f2
Petar Stoyanov <peterstoyanov83@gmail.com>
2022-10-07 22:24:01 +0200

    imported tkinter
    building a frame for the calculator

---
+++ tkinter_experiments.py
@@ -0,0 +1,58 @@
+# # from tkinter import *
+# # from tkinter import ttk
+# # root = Tk()
+# # frm = ttk.Frame(root, padding=10)
+# # frm.grid()
+# # ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+# # ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+# # root.mainloop()
+#
+# import tkinter as tk
+#
+# class App(tk.Frame):
+#     def __init__(self, master):
+#         super().__init__(master)
+#         self.pack()
+#
+#         self.entrythingy = tk.Entry()
+#         self.entrythingy.pack()
+#
+#         # Create the application variable.
+#         self.contents = tk.StringVar()
+#         # Set it to some value.
+#         self.contents.set("this is a variable")
+#         # Tell the entry widget to watch this variable.
+#         self.entrythingy["textvariable"] = self.contents
+#
+#         # Define a callback for when the user hits return.
+#         # It prints the current value of the variable.
+#         self.entrythingy.bind('<Key-Return>',
+#                              self.print_contents)
+#
+#     def print_contents(self, event):
+#         print("Hi. The current entry content is:",
+#               self.contents.get())
+#
+#
+# root = tk.Tk()
+# myapp = App(root)
+# myapp.mainloop()
+
+import tkinter as tk
+
+class App(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+
+# create the application
+myapp = App()
+
+#
+# here are method calls to the window manager class
+#
+myapp.master.title("My Do-Nothing Application")
+myapp.master.maxsize(1000, 400)
+
+# start the program
+myapp.mainloop()
